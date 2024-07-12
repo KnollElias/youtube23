3fa3d5c36f33632e1df51ee93e5622064cffcc31
KnollElias <107244530+KnollElias@users.noreply.github.com>
2024-07-13 00:19:35 +0200

    Complete register page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -47,8 +47,8 @@ function Register() {
     <div className="register">
       <form onSubmit={handleSubmit}>
         <div className="left">
-          <h1>Create a new account</h1>
-          <label htmlFor="">Username</label>
+          <h1>Neuen Account erstellen</h1>
+          <label htmlFor="">Benutzername</label>
           <input
             name="username"
             type="text"
@@ -62,36 +62,36 @@ function Register() {
             placeholder="email"
             onChange={handleChange}
           />
-          <label htmlFor="">Password</label>
+          <label htmlFor="">Passwort</label>
           <input name="password" type="password" onChange={handleChange} />
-          <label htmlFor="">Profile Picture</label>
+          <label htmlFor="">Profil Foto</label>
           <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-          <label htmlFor="">Country</label>
+          <label htmlFor="">Land</label>
           <input
             name="country"
             type="text"
             placeholder="Usa"
             onChange={handleChange}
           />
-          <button type="submit">Register</button>
+          <button type="submit">Registrieren</button>
         </div>
         <div className="right">
-          <h1>I want to become a seller</h1>
+          <h1>Ich möchte Dienstleistungen anbieten</h1>
           <div className="toggle">
-            <label htmlFor="">Activate the seller account</label>
+            <label htmlFor="">Werde Service Provider</label>
             <label className="switch">
               <input type="checkbox" onChange={handleSeller} />
               <span className="slider round"></span>
             </label>
           </div>
-          <label htmlFor="">Phone Number</label>
+          <label htmlFor="">Telefon Nummer</label>
           <input
             name="phone"
             type="text"
             placeholder="+1 234 567 89"
             onChange={handleChange}
           />
-          <label htmlFor="">Description</label>
+          <label htmlFor="">Profiloe Beschribung</label>
           <textarea
             placeholder="A short description of yourself"
             name="desc"
